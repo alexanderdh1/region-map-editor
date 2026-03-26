691821c8aa51e517bac61824aeceec627d3c7d51
Alexanderdh1 <alexander@hougaard.org>
2026-03-26 16:56:14 +0100

    Removed dead code

--- include/ui/UILayer.h
+++ include/ui/UILayer.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <string>
 #include "core/Core.h"
 #include "math/Vec2.h"
 
@@ -21,18 +19,10 @@ private:
     void renderPopup(Core& core);
     void renderContextMenu(Core& core);
 
-    // Right sidebar state
-    // (no extra state needed yet)
-
     // Left sidebar state
-    bool treeExpanded_      = false;
-    bool openColourPicker_  = false;
+    bool treeExpanded_ = false;
 
     // ImGui text field focus tracking
     bool nameFieldActive_ = false;
     bool noteFieldActive_ = false;
-
-    // Legacy click zones (kept for structure, unused)
-    struct ClickZone { double x, y, w, h; int childIndex; };
-    std::vector<ClickZone> subRegionZones_;
 };
